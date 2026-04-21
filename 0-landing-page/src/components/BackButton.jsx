f1c9d87f44a9735cf6f3765cd3757096b2bd3fb0
Alex Jarabi <jarabialex@gmail.com>
2026-04-21 12:07:30 +0300

    configure vite.config.js for gh-pages

---
+++ 0-landing-page/src/components/BackButton.jsx
@@ -0,0 +1,9 @@
+import './BackButton.css';
+
+const BackButton = () => (
+    <a href='/frontend-features/' className='back-button'>
+        ← Back to Features
+    </a>
+);
+
+export default BackButton;
--- 4-toast-notifications/vite.config.js
+++ 4-toast-notifications/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/frontend-features/4-toast-notifications/',
+});
